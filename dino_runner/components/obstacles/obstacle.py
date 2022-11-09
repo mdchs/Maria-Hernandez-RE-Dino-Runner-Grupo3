3ec3eee6136ac23d1317e25d2758805455decf96
mdchs <Maria.Hernandez@Fundacion-jala.org>
2022-11-08 19:46:09 -0600

    "Obstacle files"

---
+++ dino_runner/components/obstacles/obstacle.py
@@ -0,0 +1,20 @@
+from pygame.sprite import Sprite
+
+from dino_runner.utils.constants import SCREEN_WIDTH
+
+class Obstacle(Sprite):
+    def __init__(self, image, type):
+        self.image = image
+        self.type = type
+        self.rect = self.image[self.type].get_rect()
+        self.rect.x = SCREEN_WIDTH
+
+    def update(self, obstacle):
+        self.rect.x -= 5
+        if self.rect.x < -self.rect.width:
+            obstacle.pop()
+
+    def draw(self, screen):
+        screen.blit(self.image[self.type], self.rect)
+
+
